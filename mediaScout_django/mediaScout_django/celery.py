ce6079c58a974597e7db028ef9dd479308502189
Zino Mahmoud  (LWN) <Mahmoud.Zino@liebherr.com>
2023-01-17 15:28:58 +0100

    updated broker link

--- mediaScout_django/mediaScout_django/celery.py
+++ mediaScout_django/mediaScout_django/celery.py
@@ -5,14 +5,18 @@ from celery.schedules import crontab
 
 from api.tasks import test_task
 
+# load django default settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediaScout_django.settings')
 
+# add rabbitmq as broker
 app = Celery('mediaScout_django', broker='amqp://guest:guest@rabbitmq:5672/')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+# discover tasks with the tag @shared_task
 app.autodiscover_tasks()
 
+# configure tasks
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(10.0, test_task, name='add every 10')
